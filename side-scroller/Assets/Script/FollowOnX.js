0e873d63a9f04c622b121ad1e79c36dd66384e27
to-miyake <rascal.miyake@gmail.com>
2014-01-25 14:20:34 +0900

    del project

--- side-scroller/Assets/Script/FollowOnX.js
+++
@@ -1,6 +0,0 @@
-﻿var target:GameObject;
-
-function Update() {
-	transform.position.x = target.transform.position.x + 8;
-	transform.position.y = target.transform.position.y + 4;
-}
--- side-scroller/Assets/Script/Spin.js
+++
@@ -1,5 +0,0 @@
-﻿var speed:float = 1.0;
-
-function Update () {
-	transform.Rotate(Vector3(0,speed,0));
-}
